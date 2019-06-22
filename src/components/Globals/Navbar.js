068cadefc5c8ff35ed867c17175d2c11f802a9a9
Montbra <montbra@gmail.com>
2019-06-22 00:53:16 -0300

    starting Navbar

---
+++ src/components/Globals/Navbar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { Link } from "gatsby"
+import logo from "../../images/logo.svg"
+import { FaCartArrowDown } from 'react-icons/fa'
+
+export default class Navbar extends Component {
+  state = {
+    navbarOpen: false,
+    css: "collapse navbar-collapse",
+    links: [
+      {
+        id: 1,
+        path: "/",
+        text: "home",
+      },
+      {
+        id: 2,
+        path: "/about",
+        text: "about",
+      },
+    ],
+  }
+
+  navbarHandler = () => {
+    console.log('hello')
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-light navbar-light">
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="logo" />
+          {/* https://www.iconfinder.com/icons/185113/coffee_streamline_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/webalys */}
+        </Link>
+      </nav>
+    )
+  }
+}
+
+
