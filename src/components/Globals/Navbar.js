ff5d59e200a7f834e5f7124a117143d5cec6fd85
Montbra <montbra@gmail.com>
2019-06-22 15:03:04 -0300

    Navbar added and basic CSS

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -22,7 +22,9 @@ export default class Navbar extends Component {
   }
 
   navbarHandler = () => {
-    console.log('hello')
+    this.state.navbarOpen
+      ? this.setState({ navbarOpen: false, css: "collapse navbar-collapse" })
+      : this.setState({ navbarOpen: true, css: "collapse navbar-collapse show" })
   }
 
   render() {
@@ -34,6 +36,26 @@ export default class Navbar extends Component {
 Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/webalys */}
         </Link>
+        <button className="navbar-toggler" type="button" onClick={this.navbarHandler}>
+          <span className="navbar-toggler-icon"/>
+        </button>
+        <div className={this.state.css}>
+          <ul className="navbar-nav mx-auto">
+            {
+              this.state.links.map(link => {
+                return (
+                  <li key={link.id} className="nav-item">
+                    <Link to={link.path}className="nav-link text-capitalize">{link.text}
+                    </Link>
+                  </li>
+                )
+              })        
+            }
+            <li className="nav-item ml-sm-5">
+              <FaCartArrowDown className="cart-icon"/>
+            </li>
+          </ul>
+        </div>
       </nav>
     )
   }
