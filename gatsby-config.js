34b34ec82b2a9511abb34fbce844ef4fc10caf54
Montbra <montbra@gmail.com>
2019-06-24 22:40:13 -0300

    change theme and background color

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ff9d0a`,
+        theme_color: `#ff9d0a`,
         display: `minimal-ui`,
         icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
